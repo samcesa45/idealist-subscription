1336e14ce7b3a0e1e37b6a42ea1f2b44e851d94d
Samuel <samcesa45@gmail.com>
2024-04-24 11:25:40 +0100

    added feature1, feature2 pages

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Database\Seeders;
+
+use \App\Models\Package;
+use \App\Models\Feature;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'user1',
+             'email' => 'user1@mailinator.com',
+             'password' => bcrypt('password123')
+         ]);
+
+         Feature::create([
+            'route_name'=> 'feature1.index',
+            'image'=>'https://static-00.iconduck.com/assets.00/plus-icon-2048x2048-z6v59bd6.png',
+            'name'=>'Calculate Sum',
+            'description' => 'Calculate difference of two numbers',
+            'required_credits'=> 1,
+            'active' => true,
+         ]);
+
+         Feature::create([
+            'route_name'=> 'feature2.index',
+            'image'=>'https://static-00.iconduck.com/assets.00/plus-icon-2048x2048-z6v59bd6.png',
+            'name'=>'Calculate Sum',
+            'description' => 'Calculate difference of two numbers',
+            'required_credits'=> 3,
+            'active' => true,
+         ]);
+
+         Package::create([
+            'name' => 'Basic',
+            'price' => 5,
+            'credits' => 20
+
+         ]);
+
+         Package::create([
+            'name' => 'Silver',
+            'price' => 20,
+            'credits' => 100
+
+         ]);
+
+         Package::create([
+            'name' => 'Gold',
+            'price' => 50,
+            'credits' => 500
+
+         ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Feature1Controller;
+use App\Http\Controllers\Feature2Controller;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/feature1', [Feature1Controller::class, 'index'])->name('feature1.index');
+    Route::get('/feature2', [Feature2Controller::class, 'index'])->name('feature2.index');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
